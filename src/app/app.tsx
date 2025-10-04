09e70645cfd0c65a944b16759aed1c95ae8d3db1
Emmo00 <nwaforemmanuel005@gmail.com>
2025-10-04 09:26:33 +0100

    refactor: simplify App component by removing dynamic import of AppComponent

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { APP_NAME } from "@/lib/constants";
-
-// note: dynamic import is required for components that use the Frame SDK
-const AppComponent = dynamic(() => import("@/components/App"), {
-  ssr: false,
-});
+import AppComponent from "@/components/App";
 
 export default function App(
   { title }: { title?: string } = { title: APP_NAME }
