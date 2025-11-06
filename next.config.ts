04a48819cc4f9b75315d0b8821b9baf4c18c0141
Emmo00 <nwaforemmanuel005@gmail.com>
2025-11-06 06:03:47 +0100

    ...

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */ 
-  // Add image domains if needed
-  images: {
-    domains: ['fonts.googleapis.com'],
-  },
 };
 
 export default nextConfig;
